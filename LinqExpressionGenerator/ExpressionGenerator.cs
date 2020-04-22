032ffa6e5063a46fdd2a7a9575e6bed492979ad6
levent uysal <leventuysal@outlook.com>
2020-04-22 15:06:45 +0300

    Bug fix

--- LinqExpressionGenerator/ExpressionGenerator.cs
+++ LinqExpressionGenerator/ExpressionGenerator.cs
@@ -20,32 +20,32 @@ namespace LinqExpressionGenerator
                 if (typeof(string).IsAssignableFrom(item.PropertyType) && item.GetValue(request, null) == null)
                     continue;
 
-                if (typeof(int).IsAssignableFrom(item.PropertyType) && (item.GetValue(request, null) == null || (int)item.GetValue(request, null) == 0))
+                if ((typeof(int).IsAssignableFrom(item.PropertyType)|| typeof(int?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (int)item.GetValue(request, null) == 0))
                     continue;
 
-                if (typeof(decimal).IsAssignableFrom(item.PropertyType) && (item.GetValue(request, null) == null || (decimal)item.GetValue(request, null) == 0))
+                if ((typeof(short).IsAssignableFrom(item.PropertyType) || typeof(short?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (int)item.GetValue(request, null) == 0))
                     continue;
 
-                if (typeof(double).IsAssignableFrom(item.PropertyType) && (item.GetValue(request, null) == null || (double)item.GetValue(request, null) == 0))
+                if ((typeof(decimal).IsAssignableFrom(item.PropertyType)|| typeof(decimal?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (decimal)item.GetValue(request, null) == 0))
                     continue;
 
-                if (typeof(DateTime).IsAssignableFrom(item.PropertyType) && item.GetValue(request, null) == null)
+                if ((typeof(double).IsAssignableFrom(item.PropertyType)|| typeof(double?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (double)item.GetValue(request, null) == 0))
                     continue;
 
-                if (typeof(DateTime?).IsAssignableFrom(item.PropertyType) && item.GetValue(request, null) == null)
+                if ((typeof(DateTime).IsAssignableFrom(item.PropertyType)|| typeof(DateTime?).IsAssignableFrom(item.PropertyType)) && item.GetValue(request, null) == null)
                     continue;
 
                 //when model property was string, called object does getting lowercase
                 if (typeof(string).IsAssignableFrom(item.PropertyType))
                 {
                     methodInfo = typeof(string).GetMethod("Contains", new Type[] { typeof(string) });
-                    var argumentsForStringObject = Expression.Constant(item.GetValue(request, null).ToString().ToLower());
-                    var propertyForStringObject = Expression.Property(parameter, item.Name);
-                    var stringObjectWithLowerCase = Expression.Call(propertyForStringObject, "ToLower", null);
-                    expression = Expression.Call(stringObjectWithLowerCase, methodInfo, argumentsForStringObject);
+                    var arguments = Expression.Constant(item.GetValue(request, null).ToString().ToLower());
+                    var property = Expression.Property(parameter, item.Name);
+                    var stringObjectWithLowerCase = Expression.Call(property, "ToLower", null);
+                    expression = Expression.Call(stringObjectWithLowerCase, methodInfo, arguments);
                 }
                 //if you need to filter between two date, you have to set properties names to be 'TrhBasTarih','TrhBitTarih' 
-                else if ((typeof(DateTime).IsAssignableFrom(item.PropertyType) || typeof(DateTime?).IsAssignableFrom(item.PropertyType)) && item.Name.Split('_').Length > 0)
+                else if ((typeof(DateTime).IsAssignableFrom(item.PropertyType) || typeof(DateTime?).IsAssignableFrom(item.PropertyType)) && item.Name.Split('_').Length > 1)
                 {
                     var splittedDate = item.Name.Split('_');
                     var propDate = Expression.Property(parameter, splittedDate[0]);
@@ -58,6 +58,13 @@ namespace LinqExpressionGenerator
                         Expression.LessThanOrEqual(Expression.MakeMemberAccess(Expression.Convert(propDate, typeof(DateTime)), typeof(DateTime).GetMember("Date").Single()), Expression.Convert(constDate, typeof(DateTime))) :
                         Expression.AndAlso(finalExpression, parameter);
                 }
+                else if ((typeof(DateTime).IsAssignableFrom(item.PropertyType) || typeof(DateTime?).IsAssignableFrom(item.PropertyType)))
+                {
+                    Expression property = Expression.PropertyOrField(parameter, item.Name);
+                    Expression arguments = Expression.Constant(item.GetValue(request, null));
+                    methodInfo = typeof(DateTime).GetMethod("Equals", new Type[] { typeof(DateTime) });
+                    expression = Expression.Call(Expression.MakeMemberAccess(Expression.Convert(property, typeof(DateTime)), typeof(DateTime).GetMember("Date").Single()), methodInfo,arguments);
+                }
                 else
                 {
                     if (typeof(int).IsAssignableFrom(item.PropertyType))
@@ -66,9 +73,8 @@ namespace LinqExpressionGenerator
                         methodInfo = typeof(decimal).GetMethod("Equals", new Type[] { typeof(decimal) });
                     if (typeof(double).IsAssignableFrom(item.PropertyType))
                         methodInfo = typeof(double).GetMethod("Equals", new Type[] { typeof(double) });
-                    if(typeof(DateTime).IsAssignableFrom(item.PropertyType))
-                        methodInfo = typeof(DateTime).GetMethod("Equals", new Type[] { typeof(DateTime) });
-
+                    if (typeof(short).IsAssignableFrom(item.PropertyType))
+                        methodInfo = typeof(short).GetMethod("Equals", new Type[] { typeof(short) });
 
                     var arguments = Expression.Constant(item.GetValue(request, null));
                     var property = Expression.Property(parameter, item.Name);
