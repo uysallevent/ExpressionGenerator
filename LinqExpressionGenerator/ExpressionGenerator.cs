5dfe7f1efe70a15914083d653ce1c336bcd2c8dd
levent uysal <leventuysal@outlook.com>
2020-04-27 10:25:57 +0300

    Bug fix

--- LinqExpressionGenerator/ExpressionGenerator.cs
+++ LinqExpressionGenerator/ExpressionGenerator.cs
@@ -17,22 +17,22 @@ namespace LinqExpressionGenerator
             Expression expression = null;
             foreach (var item in request.GetType().GetProperties().ToList())
             {
-                if (typeof(string).IsAssignableFrom(item.PropertyType) && item.GetValue(request, null) == null)
+                if (typeof(string).IsAssignableFrom(item.PropertyType) && string.IsNullOrEmpty(item.GetValue(request, null).ToString()))
                     continue;
 
-                if ((typeof(int).IsAssignableFrom(item.PropertyType)|| typeof(int?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (int)item.GetValue(request, null) == 0))
+                if ((typeof(int).IsAssignableFrom(item.PropertyType) || typeof(int?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (int)item.GetValue(request, null) == 0))
                     continue;
 
-                if ((typeof(short).IsAssignableFrom(item.PropertyType) || typeof(short?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (int)item.GetValue(request, null) == 0))
+                if ((typeof(short).IsAssignableFrom(item.PropertyType) || typeof(short?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (short)item.GetValue(request, null) ==  0))
                     continue;
 
-                if ((typeof(decimal).IsAssignableFrom(item.PropertyType)|| typeof(decimal?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (decimal)item.GetValue(request, null) == 0))
+                if ((typeof(decimal).IsAssignableFrom(item.PropertyType) || typeof(decimal?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (decimal)item.GetValue(request, null) == 0))
                     continue;
 
-                if ((typeof(double).IsAssignableFrom(item.PropertyType)|| typeof(double?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (double)item.GetValue(request, null) == 0))
+                if ((typeof(double).IsAssignableFrom(item.PropertyType) || typeof(double?).IsAssignableFrom(item.PropertyType)) && (item.GetValue(request, null) == null || (double)item.GetValue(request, null) == 0))
                     continue;
 
-                if ((typeof(DateTime).IsAssignableFrom(item.PropertyType)|| typeof(DateTime?).IsAssignableFrom(item.PropertyType)) && item.GetValue(request, null) == null)
+                if ((typeof(DateTime).IsAssignableFrom(item.PropertyType) || typeof(DateTime?).IsAssignableFrom(item.PropertyType)) && item.GetValue(request, null) == null)
                     continue;
 
                 //when model property was string, called object does getting lowercase
@@ -63,7 +63,7 @@ namespace LinqExpressionGenerator
                     Expression property = Expression.PropertyOrField(parameter, item.Name);
                     Expression arguments = Expression.Constant(item.GetValue(request, null));
                     methodInfo = typeof(DateTime).GetMethod("Equals", new Type[] { typeof(DateTime) });
-                    expression = Expression.Call(Expression.MakeMemberAccess(Expression.Convert(property, typeof(DateTime)), typeof(DateTime).GetMember("Date").Single()), methodInfo,arguments);
+                    expression = Expression.Call(Expression.MakeMemberAccess(Expression.Convert(property, typeof(DateTime)), typeof(DateTime).GetMember("Date").Single()), methodInfo, arguments);
                 }
                 else
                 {
